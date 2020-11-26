d14b48ead243fe7e844c7b997d1d3a3dce736990
Željko Filipin <zeljko.filipin@gmail.com>
2020-11-26 19:55:59 +0100

    Update server

--- server.js
+++ server.js
@@ -6,5 +6,7 @@ const server = ronin.server()
 
 database.connect( process.env.CONNECTIONSTRING )
 
-server.use( '/', mocks.server( server.Router(), false, false ) )
+server.use( '/foo', (req, res) => {
+  return res.json({ "foo": "bar" })
+})
 server.start()
